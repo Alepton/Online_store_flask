05502b82b94a483ce8bb04fce28fe0df21e557ce
Andrei Kardovich <akardovich@gmail.com>
2022-07-20 21:00:31 +0300

    create data base

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    discription = db.Column(db.String(150), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    isActiv = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return self.title
+
 
 
 @app.route('/')
@@ -15,6 +28,11 @@ def index():
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+
+@app.route('/creat')
+def creat():
+    return render_template("creat.html")
 
 
 @app.route('/user')
