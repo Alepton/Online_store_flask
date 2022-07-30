a7346cd9116e76f5b4b73f70d3b34875f8279af7
Andrei Kardovich <akardovich@gmail.com>
2022-07-30 21:41:00 +0300

    create card

--- app.py
+++ app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///shop.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class Item(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
-    discription = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(150), nullable=False)
     price = db.Column(db.Integer, nullable=False)
     isActiv = db.Column(db.Boolean, default=True)
 
@@ -30,9 +31,23 @@ def about():
     return render_template("about.html")
 
 
-@app.route('/creat')
-def creat():
-    return render_template("creat.html")
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        price = request.form['price']
+
+        item = Item(title=title, description=description, price=price)
+
+        try:
+            db.session.add(item)
+            db.session.commit()
+            return redirect('/') # переадресация на главную страницу после добавления данных в базу
+        except:
+            return "Произошла ошибка"
+    else:
+        return render_template("create.html")
 
 
 @app.route('/user')
